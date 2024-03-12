591e0d5d7dbb2608e79181140d2b9468537454ac
azikulov <azikulov9@gmail.com>
2024-03-12 16:14:17 +0500

    [info] auth form validation

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -16,7 +16,11 @@ export default function Signin() {
   const router = useRouter();
 
   const { updateAuth, updateToken } = useAuthStore();
-  const { register, handleSubmit } = useForm<FormData>();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>();
   const signin = useMutation({
     mutationKey: ['signin'],
     mutationFn: signinService,
@@ -58,8 +62,22 @@ export default function Signin() {
               type={'text'}
               placeholder='Введите ваш адрес электронной почты'
               className={styles['field__input']}
-              {...register('email')}
+              {...register('email', {
+                required: true,
+                pattern:
+                  /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+              })}
             />
+
+            {errors.email?.type === 'required' && (
+              <p className={styles['field__error-message']}>Вы не ввели адрес электронной почты!</p>
+            )}
+
+            {errors.email?.type === 'pattern' && (
+              <p className={styles['field__error-message']}>
+                Вы ввели неправильный адрес электронной почты!
+              </p>
+            )}
           </div>
 
           <div className={styles['field']}>
@@ -69,20 +87,26 @@ export default function Signin() {
               type={'password'}
               placeholder='Введите ваш пароль'
               className={styles['field__input']}
-              {...register('password')}
+              {...register('password', {
+                required: true,
+              })}
             />
+
+            {errors.password?.type === 'required' && (
+              <p className={styles['field__error-message']}>Вы ввели неправильный пароль!</p>
+            )}
           </div>
 
           <button disabled={signin.isLoading} className={styles['form__submit']}>
             Продолжить
           </button>
 
-          <p className={styles['form__description']}>Или продолжить с помощью</p>
+          {/* <p className={styles['form__description']}>Или продолжить с помощью</p>
 
           <button className={styles['form__button-google']}>
             <GoogleIcon />
             <span>Google</span>
-          </button>
+          </button> */}
         </form>
       </div>
 
--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,4 +1,6 @@
 'use client';
+
+import cn from 'classnames';
 import { useForm } from 'react-hook-form';
 import { useRouter } from 'next/navigation';
 import { useMutation } from 'react-query';
@@ -13,7 +15,11 @@ import styles from './page.module.scss';
 export default function Signup() {
   const router = useRouter();
 
-  const { register, handleSubmit } = useForm<FormData>();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>({});
 
   const signup = useMutation({
     mutationKey: ['signup'],
@@ -47,9 +53,17 @@ export default function Signup() {
             <input
               type={'text'}
               placeholder='Введите ваше имя'
-              className={styles['field__input']}
-              {...register('username', { required: true })}
+              className={cn(styles['field__input'], errors.username?.type && styles['error'])}
+              {...register('username', { required: true, pattern: /^[A-Za-z]*$/ })}
             />
+
+            {errors.username?.type === 'required' && (
+              <p className={styles['field__error-message']}>Вы не ввели ваше имя!</p>
+            )}
+
+            {errors.username?.type === 'pattern' && (
+              <p className={styles['field__error-message']}>Вы ввели неправильное имя!</p>
+            )}
           </div>
 
           <div className={styles['field']}>
@@ -58,9 +72,23 @@ export default function Signup() {
             <input
               type={'email'}
               placeholder='Введите ваш адрес электронной почты'
-              className={styles['field__input']}
-              {...register('email', { required: true })}
+              className={cn(styles['field__input'], errors.email?.type && styles['error'])}
+              {...register('email', {
+                required: true,
+                pattern:
+                  /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+              })}
             />
+
+            {errors.email?.type === 'required' && (
+              <p className={styles['field__error-message']}>Вы не ввели адрес электронной почты!</p>
+            )}
+
+            {errors.email?.type === 'pattern' && (
+              <p className={styles['field__error-message']}>
+                Вы ввели неправильный адрес электронной почты!
+              </p>
+            )}
           </div>
 
           <div className={styles['field']}>
@@ -69,21 +97,25 @@ export default function Signup() {
             <input
               type={'password'}
               placeholder='Введите ваш пароль'
-              className={styles['field__input']}
+              className={cn(styles['field__input'], errors.password?.type && styles['error'])}
               {...register('password', { required: true })}
             />
+
+            {errors.password?.type === 'required' && (
+              <p className={styles['field__error-message']}>Вы ввели неправильный пароль!</p>
+            )}
           </div>
 
           <button disabled={signup.isLoading} className={styles['form__submit']}>
             Создать учетную запись
           </button>
 
-          <p className={styles['form__description']}>Или продолжить с помощью</p>
+          {/* <p className={styles['form__description']}>Или продолжить с помощью</p>
 
           <button className={styles['form__button-google']}>
             <GoogleIcon />
             <span>Google</span>
-          </button>
+          </button> */}
         </form>
       </div>
 
