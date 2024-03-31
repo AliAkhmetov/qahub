8a2debc5d02c6833fdb55919762b2cdac5283d89
azikulov <azikulov9@gmail.com>
2024-03-31 12:02:33 +0500

    added seo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,25 +14,19 @@ const fontNunitoSans = Nunito_Sans({
 
 export const metadata: Metadata = {
   title: 'QAhub',
-  description: 'Просто о QA',
-  keywords: ['qa', "qahub", "qa hub", "qahub kz", "qa hub kz", "qa kz", "qa engineer", "тестирование", "Тестілеуші", "qa blog"],
+  description: 'Узнайте все о QA.',
+  keywords: 'qahub, qa engineer, тестирование, Тестілеуші, qa blog, блог о тестировании и qa',
   openGraph: {
-    title: 'Все о QA',
+    title: 'QA HUB KZ - все о QA',
     description: 'QA туралы блог, Статьи о QA в Казахстане',
     siteName: 'qahub.kz',
-    url: 'https://www.qahub.kz/',
+    images: ['/media/web/seo/logo.jpg'],
     type: 'website',
-    images: [
-      {
-        url: 'https://ucarecdn.com/b4863285-c39a-4c35-be2d-6f1887acaf66/d77486f10aab473db38c2413a4e3d74c.jpg', // Must be an absolute URL
-        width: 800,
-        height: 600,
-      }
-    ],
   },
+  icons: [{ rel: 'shortcut icon', type: 'image/jpeg', url: '/media/web/seo/logo.jpg' }],
   verification: {
-    google: "8JqOq6Fg0y7T6KrjtYok67XFH5dwgFh9WnC7MVbiDPg"
-  }
+    google: '8JqOq6Fg0y7T6KrjtYok67XFH5dwgFh9WnC7MVbiDPg',
+  },
 };
 
 export default function RootLayout({
@@ -42,8 +36,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='kz' data-theme='light'>
-      <head>
-      </head>
       <body className={fontNunitoSans.variable}>
         <Providers>{children}</Providers>
         <Analytics mode={'production'} />
